b32d74575a7234e5ea7b5f68c53056086e9412ef
maxim.kostin <dev@maxkostin.com>
2014-01-06 19:50:37 +0400

    Moved Lists SDK to a new SDK home dir in the repo.

--- lists-sdk/src/com/microsoft/opentech/office/odata/IEntityBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.opentech.office.odata;
-
-public interface IEntityBuilder<T> extends IBuilder<T> {
-    
-    public IEntityBuilder<T> set(String name, Object value);
-    
-    public IEntityBuilder<T> setMeta(String name, Object value);
-    
-}
